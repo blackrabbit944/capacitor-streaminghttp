04c5dd0c81e8d5648ea36c7816f23ba76134b896
blackrabbit <rabbitblack944@gmail.com>
2025-05-18 12:53:02 +0900

    第一个版本

--- example-app/src/js/example.js
+++
@@ -1,6 +0,0 @@
-import { StreamingHttp } from '@capacitor/streaming-http';
-
-window.testEcho = () => {
-    const inputValue = document.getElementById("echoInput").value;
-    StreamingHttp.echo({ value: inputValue })
-}
